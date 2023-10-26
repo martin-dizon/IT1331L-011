dcc1c3c139ab2f4fa73646c9e55cd73fd1ab9863
martin-dizon <martinalfonso.dizon@uap.asia>
2023-10-26 20:07:27 +0800

    put and delete api added

--- registration-api/src/main/java/com/uap/it1331l/registrationapi/controller/RegistrationController.java
+++ registration-api/src/main/java/com/uap/it1331l/registrationapi/controller/RegistrationController.java
@@ -1,10 +1,14 @@
 package com.uap.it1331l.registrationapi.controller;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +27,15 @@ public class RegistrationController {
 	@GetMapping("/attendees")
 	public List<Attendee> getAttendees() {
 		return registrationRepo.findAll();
+	}
+	@PutMapping("/event/survey/eligibility")
+	public List<Attendee> updateEligibility() {
+		registrationRepo.updateEligibility();
+		List<Attendee> attendees = registrationRepo.getAllEligible();
+		return attendees;
+	}
+	@DeleteMapping("event/registration/{id}")
+	public void deleteAttendee(@PathVariable("id") int attendeeId) {
+		registrationRepo.deleteId(attendeeId);
 	}
 }
--- registration-api/src/main/java/com/uap/it1331l/registrationapi/repository/RegistrationMybatisRepository.java
+++ registration-api/src/main/java/com/uap/it1331l/registrationapi/repository/RegistrationMybatisRepository.java
@@ -2,9 +2,11 @@ package com.uap.it1331l.registrationapi.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.uap.it1331l.registrationapi.model.Attendee;
 
@@ -14,4 +16,10 @@ public interface RegistrationMybatisRepository {
 	public int insert(Attendee attendee);
 	@Select("SELECT * FROM attendees")
 	public List<Attendee> findAll();
+	@Update("UPDATE attendees SET surveyEligibilityTag = true WHERE course = 'BSIT'")
+	public int updateEligibility();
+	@Select("SELECT * FROM attendees WHERE surveyEligibilityTag = true")
+	public List<Attendee> getAllEligible();
+	@Delete("DELETE FROM attendees WHERE attendeeId = #{attendeeId}")
+	public int deleteId(int attendeeId);
 }
